6228cf68b9f4041f0a36282d2c44c135c965b54b
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2022-06-17 21:13:58 +0530

    fixed properly

--- client/src/components/UserComp.js
+++ client/src/components/UserComp.js
@@ -1,40 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
 
+const UserComp = () => {
+  const [userIpData, setUserIpData] = useState(null);
 
-const UserComp = () =>  {
+  const getUserIpData = () => {
+    console.log("getUserIpData");
 
-    const [userIpData, setUserIpData] = useState({});
-
-    const getUserIpData = () => {
-      console.log('getUserIpData');
-
-      fetch('/client-cokkie-handler')
-      .then(res => res.json())
-      // .then(data =>  {}console.log('data from server is : ' ,  data?.ipData) );
-      .then(data => {
-        console.log('data from server is : ' ,  data?.ipData);
+    fetch("/client-cokkie-handler")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data from server is : ", data?.ipData);
         setUserIpData(data?.ipData);
-      })
+      });
+  };
 
-    }
-    
   return (
     <div>
-     <h2> User Ip Data will display Below :</h2>
-     {
-         userIpData && (
-             <>
-              {JSON.stringify(userIpData)}
-            </>
-         )
-     }
-  
-      <button onClick={() => getUserIpData()} > Click to see Ip Data </button>
+      <h2> User Ip Data will display Below :</h2>
+      {userIpData && (
+        <div className="user-data-display">
+          <div className="user-label-value">
+            <span className="user-data-label">Ip Address :</span>
+            <span className="user-data-value">{userIpData?.ip}</span>
+          </div>
+
+          <div className="user-label-value">
+            <span className="user-data-label">Country : </span>
+            <span className="user-data-value"> {userIpData?.country}</span>
+          </div>
+
+          <div className="user-label-value">
+            <span className="user-data-label"> City : </span>
+            <span className="user-data-value">{userIpData?.city} </span>
+          </div>
+
+          <div className="user-label-value">
+            <span className="user-data-label"> Region : </span>
+            <span className="user-data-value"> {userIpData?.region} </span>
+          </div>
+        </div>
+      )}
+
+      <button onClick={() => getUserIpData()}> Click to see Ip Data </button>
     </div>
-
-  )
-}
-
+  );
+};
 
 export default UserComp;
